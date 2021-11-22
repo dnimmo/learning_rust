b4996492499eed44e18607d52ea0665d2ae5d2b8
Nimmo <dnimmo@gmail.com>
2021-11-22 21:37:57 +0000

    print response after comparison

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,9 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("PLease input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
@@ -11,5 +15,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess)
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
